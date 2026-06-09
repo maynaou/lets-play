53888c2eb335bab4e52f35dd4309e7ad8bee01a5
maynaou <mohssinaynaou874@gmail.com>
2026-06-09 17:06:44 +0100

    first commit

---
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,21 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner defaultTest() {
+		return args -> System.out.println("Hello1 World");
+	}
+
+}
